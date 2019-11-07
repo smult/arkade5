5448c27d239da7807bceaa6cd73d76a25547e476
Anastasiya Bukhala <anastasiya@arkitektum.no>
2019-11-07 14:13:13 +0100

    ArchivePart class added
    ARKADE-389

---
+++ src/Arkivverket.Arkade.Core/Util/ArchivePart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arkivverket.Arkade.Core.Util
+{
+    public abstract class ArchivePart
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
