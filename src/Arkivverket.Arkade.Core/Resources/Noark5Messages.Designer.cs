276eac9e4a2f944944746154e5689aaad5c0d00e
Anastasiya Bukhala <anastasiya@arkitektum.no>
2019-11-08 13:18:12 +0100

    Archive part name added to N5_10 test report
    ARKADE-389

--- src/Arkivverket.Arkade.Core/Resources/Noark5Messages.Designer.cs
+++ src/Arkivverket.Arkade.Core/Resources/Noark5Messages.Designer.cs
@@ -70,7 +70,7 @@ namespace Arkivverket.Arkade.Core.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Arkivdel (systemID): {0}.
+        ///   Looks up a localized string similar to Arkivdel (systemID - tittel): {0} - {1}.
         /// </summary>
         internal static string ArchivePartSystemId {
             get {
